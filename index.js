4e30f13fe65d639fc39a0046790261dc3295c14d
edu-ti <educs85.ti@gmail.com>
2024-10-09 18:52:18 -0300

    Atualização no código

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
 //Desafio Classificador de nível de Herói
 
 let nome = "Kratos"
-let xp = "30.900"
+let xp = 1
 
-
-if (xp <= "1.000") {
+if (xp <= 1000) {
     console.log("O Herói de nome " + nome + " está no nível de Ferro")
-} 
-else if (xp >= "1.001" && xp <= "2.000") {
+}
+else if (xp >= 1001 && xp <= 2000) {
     console.log("O Herói de nome " + nome + " está no nível de Bronze")
-} 
-else if (xp >= "2.001" && xp <= "5.000") {
+}
+else if (xp >= 2001 && xp <= 5000) {
     console.log("O Herói de nome " + nome + " está no nível de Prata")
-} 
-else if (xp >= "5.001" && xp <= "7.000") {
+}
+else if (xp >= 5001 && xp <= 7000) {
     console.log("O Herói de nome " + nome + " está no nível de Ouro")
-} 
-else if (xp >= "7.001" && xp <= "8.000") {
+}
+else if (xp >= 7001 && xp <= 8000) {
     console.log("O Herói de nome " + nome + " está no nível de Platina")
-} 
-else if (xp >= "8.001" && xp <= "9.000") {
+}
+else if (xp >= 8001 && xp <= 9000) {
     console.log("O Herói de nome " + nome + " está no nível de Ascendente")
-} 
-else if (xp >= "9.001" && xp <= "10.000") {
+}
+else if (xp >= 9001 && xp <= 10000) {
     console.log("O Herói de nome " + nome + " está no nível de Imortal")
-} 
-else if(xp >= "10.001") {
+}
+else if (xp >= 10001) {
     console.log("O Herói de nome " + nome + " está no nível de Radiante")
 }
-
